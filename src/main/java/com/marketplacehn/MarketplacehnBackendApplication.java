bbe3aff044e95205c0c057047a3533f58a974aaf
Igor A. Zelaya <izelaya@baas.hn>
2022-12-28 14:56:07 -0600

    Initial commit

---
+++ src/main/java/com/marketplacehn/MarketplacehnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marketplacehn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplacehnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplacehnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marketplacehn/MarketplacehnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplacehn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacehnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
