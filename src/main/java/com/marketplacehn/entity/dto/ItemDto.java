140845ec1b96d457b25d4c939ed3ea6f6c831ebc
Gabriel Zelaya <gabriel.zelayaa21@gmail.com>
2023-11-19 15:24:40 -0600

    Item and Bid dtos populated

--- src/main/java/com/marketplacehn/entity/dto/ItemDto.java
+++ src/main/java/com/marketplacehn/entity/dto/ItemDto.java
@@ -1,4 +1,17 @@
 package com.marketplacehn.entity.dto;
 
-public record ItemDto() {
-}
+import com.marketplacehn.entity.Bid;
+import com.marketplacehn.entity.User;
+import com.marketplacehn.entity.enums.ItemSellType;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public record ItemDto(
+        User itemOwner,
+        BigDecimal itemStartingPrice,
+        Set<Bid> itemBids,
+        String itemDeliveryAddress,
+        ItemSellType itemSellType,
+        Set<String> itemPhotos
+) {}
